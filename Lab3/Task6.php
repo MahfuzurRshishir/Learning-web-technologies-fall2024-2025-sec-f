aa7e73e2b39567e3183e957eec67504bc72270a8
Mahfuzur Rahman <158088815+MahfuzurRshishir@users.noreply.github.com>
2024-11-11 16:26:09 +0600

    Task6.php

--- Lab3/Task6.php
+++ Lab3/Task6.php
@@ -1,1 +1,19 @@
+<?php
+$arr = array(1,2,3,45,66);
+$searchKey = 100;
+$flag = false;
 
+for($i=0; $i<count($arr); $i++){
+    if($arr[$i]===$searchKey){
+        $flag= true;
+        break;
+    }
+}
+if($flag===true){
+    echo "The element is found";
+
+}else{
+    echo "The element is not found";
+}
+
+?>

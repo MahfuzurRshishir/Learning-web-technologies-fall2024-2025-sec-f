a78d5a005a85afa416c699b5771d87c70e9e0ec2
Mahfuzur Rahman <158088815+MahfuzurRshishir@users.noreply.github.com>
2024-11-11 16:26:34 +0600

    Task7.php

--- Lab3/Task7.php
+++ Lab3/Task7.php
@@ -1,1 +1,29 @@
+<?php
 
+for($i=1; $i<=3; $i++){
+    for($j=1; $j<=$i; $j++){
+        echo "*";
+    }
+    echo "<br>";
+}
+echo"-----------<br>";
+for($i=3; $i>0; $i--){
+    $num=1;
+    for($j=1; $j<=$i; $j++){
+        echo $num;
+        $num++;
+    }
+    echo "<br>";
+}
+echo"-----------<br>";
+$letter= 'A';
+for($i=1; $i<=3; $i++){
+    
+    for($j=1; $j<=$i; $j++){
+        echo $letter;
+        $letter++;
+    }
+    echo "<br>";
+}
+
+?>

b770a266e555f99f59cfb51aaee61b5c0a4a5648
Mahfuzur Rahman <158088815+MahfuzurRshishir@users.noreply.github.com>
2024-11-11 16:25:19 +0600

    Task4.php

--- Lab3/Task4.php
+++ Lab3/Task4.php
@@ -1,1 +1,19 @@
+<?php
+$a=30;
+$b=40;
+$c=10;
 
+if($a>$b){
+    if($a>$c){
+        echo "The largest number is A";
+    }else{
+        echo "The largest number is C";
+    }
+}else if($b>$a){
+    if($b>$c){
+        echo "The largest number is B";
+    }else{
+        echo "The largest number is C";
+    }
+}
+?>

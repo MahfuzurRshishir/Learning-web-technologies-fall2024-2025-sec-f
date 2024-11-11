0ed642265e68846c543823ccc728570a8312962b
Mahfuzur Rahman <158088815+MahfuzurRshishir@users.noreply.github.com>
2024-11-11 16:23:50 +0600

    Task1.php

--- Lab3/Task1.php
+++ Lab3/Task1.php
@@ -1,1 +1,17 @@
+<?php
 
+$l = 10;
+$w = 20;
+
+function area($a,$b){
+    return $a*$b;
+}
+function perimeter($a,$b){
+    return 2*($a+$b);
+}
+
+echo "The area of rectangle is: ". area($l,$w);
+echo "<br>";
+echo "The perimeter of rectangle is: ". perimeter($l,$w);
+
+?>
